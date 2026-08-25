3705620ee654f345226c9ac6c7e5b9899957febd
Alan-krita <pathakalankrita2601@gmail.com>
2026-08-25 20:30:50 +0530

    Update config files: add config.js and remove config.example.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
-let SUPABASE_URL = "https://your-project.supabase.co"; // Replace with your Supabase URL
-let SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5c..."; // Replace with your Supabase Anon Key
-let TABLE_NAME = "orders"; // Replace with your table name
+// CONFIGURATION
+let SUPABASE_URL = "https://pkwoweaiztetzasqukip.supabase.co";
+let SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrd293ZWFpenRldHphc3F1a2lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4Mjc2MjQsImV4cCI6MjA5OTQwMzYyNH0.ZY4g45X8YvPhu5y6D5MXHnglD3Obx9jMllMaVLX45ck";
+let POSTMAN_API_KEY = "https://pkwoweaiztetzasqukip.supabase.co/rest/v1/rpc/get_sales_dashboard";
+let TABLE_NAME = "orders";
 
-// Dashboard Global State (Do not modify)
-let isLiveMode = false;
 let currentDataset = [];
 let filteredDataset = [];
-let selectedMonthFilter = "All";
+let isLiveMode = true;
+let selectedMonthFilter = "June 2026";
 
 const USER_NAME_MAP = {
   1001: "Faizan", 1002: "Talha", 1003: "Nidhi", 1004: "Bhageshri", 1005: "Sanika", 1006: "Prabhat", 1007: "Farooq", 1008: "Aarav", 1009: "Rhea", 1010: "Ananya",
